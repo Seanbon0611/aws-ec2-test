17dd0e00958760f224d7e92d4499cf11d8e07245
Seanbon0611 <seand0611@gmail.com>
2021-05-21 13:30:52 -0700

    initiating repo

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+
+const users = [
+  {
+    name: "Sean",
+    occupation: "Software Engineer",
+    hobbies: ["lifting", "reading", "programming"],
+  },
+  {
+    name: "Zach",
+    occupation: "Electrician",
+    hobbies: ["Math", "Football"],
+  },
+];
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send(users);
+});
+
+module.exports = router;
